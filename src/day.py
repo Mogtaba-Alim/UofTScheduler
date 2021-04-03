0a88bcf355dc500945a34bb286c786607f83d24f
Mogtaba <alim.mogtaba@gmail.com>
2021-04-03 19:07:38 -0400

    Fixed in day and week files

--- src/day.py
+++ src/day.py
@@ -1,29 +1,23 @@
 """ This is where the day class is implemented"""
 
 import tree
+from typing import Optional, List, Any
 
 
-class Day:
+class Day(tree.Tree):
     """ This class represents a single day in the schedule
     This class uses recursive tree data structures to store information
-    Private Instance Attributes:
-        - _day: A tree with trees corresponding to the hours of the day
-
     """
-    _day = tree.Tree
 
-    def __init__(self, day_of_week: str) -> None:
+    def __init__(self, root: Optional[Any], subtrees: List[tree.Tree]) -> None:
         """
         Preconditions
-            - day_of_week in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', \
+            - root in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', \
             'Sunday']
         """
-        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        if day_of_week not in days:
-            raise ValueError("Not a day of the week")
-        self._day = tree.Tree(day_of_week, [])
+        super().__init__(root, subtrees)
         for i in range(24):
-            self._day.create_new_subtree((str(i) + ":00", str(i) + ":59", "Empty", 0))
+            self.create_new_subtree((str(i) + ":00", str(i) + ":59", "Empty", 0))
 
     def identify_day(self) -> str:
         """Returns what the current day is"""
@@ -31,4 +25,4 @@ class Day:
 
     def insert_event(self, event_name: str, importance_level, start: int, end: int) -> str:
         """Insert a new event into this day"""
-        for hour in self._day
+        # for hour in self._day
--- src/week.py
+++ src/week.py
@@ -1,24 +1,19 @@
 """ This is where the week class is implemented"""
 import tree
 import day
+from typing import Optional, List, Any
 
 
-class Week:
+class Week(tree.Tree):
     """ This class representes a single week in the schedule
     This class uses recursive tree data structures to store information
     """
-    # Private Instance Atrribute:
-    #   - _days: A list with its subtrees being 7 day classes, corresponding to the days of the week
-    #   - _week: week designation
-    _week = int
-    _days = list[day.Day]
 
-    def __init__(self, week_number: int = 0) -> None:
+    def __init__(self, subtrees: List[tree.Tree], root: Optional[Any] = "Week") -> None:
+        super().__init__(root, subtrees)
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        self._week = week_number
-        self._days = []
         for i in range(7):
-            self._days.append(day.Day(days[i]))
+            self.add_new_subtree(day.Day(days[i], []))
 
     def add_event_date(self, event_name: str, day_of_week: str, start_time: int, end_time: int,
                        importance: int) -> str:
@@ -32,7 +27,7 @@ class Week:
             - start_time: The time of the day corresponding to the start time of the new event
             - end_time: The time of the day corresponding to the end time of the new event
         """
-        for days in self._days:
-            if days.identify_day() == day_of_week:
+        # for days in self._days:
+        #     if days.identify_day() == day_of_week:
 
 
