4eaf42c615bbcbd6da64475738b9ad730290b420
Mouaid Alim <mouaid@Mouaids-MacBook-Pro.local>
2021-04-14 18:43:10 -0400

    made visualization file

---
+++ src/visualization.py
@@ -0,0 +1,58 @@
+from typing import Tuple
+from tkinter import *
+
+import pygame
+from pygame.colordict import THECOLORS
+from week import Week
+
+
+def initialize_screen(allowed: list) -> pygame.Surface:
+    """Initialize pygame and the display window.
+
+    allowed is a list of pygame event types that should be listened for while pygame is running.
+    """
+    pygame.display.init()
+    pygame.font.init()
+    screen = pygame.display.set_mode((1000, 1000))
+    screen.fill(THECOLORS['white'])
+    pygame.display.flip()
+
+    pygame.event.clear()
+    pygame.event.set_blocked(None)
+    pygame.event.set_allowed([pygame.QUIT] + allowed)
+
+    return screen
+
+
+def draw_text(screen: pygame.Surface, text: str, pos: tuple[int, int]) -> None:
+    """Draw the given text to the pygame screen at the given position.
+
+    pos represents the *upper-left corner* of the text.
+    """
+    font = pygame.font.SysFont('inconsolata', 22)
+    text_surface = font.render(text, True, THECOLORS['black'])
+    width, height = text_surface.get_size()
+    screen.blit(text_surface,
+                pygame.Rect(pos, (pos[0] + width, pos[1] + height)))
+
+
+def draw_grid(screen: pygame.Surface) -> None:
+    """Draws a square grid on the given surface.
+
+    The drawn grid has GRID_SIZE columns and rows.
+    You can use this to help you check whether you are drawing nodes and edges in the right spots.
+    """
+    color = THECOLORS['grey']
+    width, height = screen.get_size()
+
+    for col in range(1, ):
+        x = col * (width // 8)
+        pygame.draw.line(screen, color, (x, 0), (x, height))
+
+    for row in range(1, 8):
+        y = row * (height // 8)
+        pygame.draw.line(screen, color, (0, y), (width, y))
+
+
+def draw_schedule(screen: pygame.surface, Schedule: Week) -> None:
+    """create the final schedule"""
