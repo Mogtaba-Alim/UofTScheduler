8e9dad0b5871fe37bc832c334e9ac10bcd756f69
Wasee Alam <71355734+WaseeA@users.noreply.github.com>
2021-04-16 09:23:14 -0400

    Update week.py

--- src/week.py
+++ src/week.py
@@ -113,7 +113,8 @@ class Week:
         """this """
         c = Calendar()
         e = Event()
-
+        
+        # line 117 - 123: https://stackoverflow.com/questions/17277002/how-to-get-all-datetime-instances-of-the-current-week-given-a-day
         today = datetime.datetime.today()
         day_of_week = today.isocalendar()[2] - 1
         start_date = today - datetime.timedelta(days=day_of_week)
